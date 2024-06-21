3ba0cb5d5e81351a7c8ee0844d34143930d6bb8a
kisal <kisal.2018368@iit.ac.lk>
2024-06-21 11:31:16 +0530

    1st commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>App Component</h2>
+  <button class="btn btn-success"> hello </button>
+  <i class="fa fa-home text-success fa-4x"></i>
+</template>
+
+<script>
+
+</script>
+
+<style>
+  @import "./styles.css";
+</style>
